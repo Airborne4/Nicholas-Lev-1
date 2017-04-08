35bc3d68f2294e0746fd20bf74930cd0c25981bf
Airborne4 <league@WTS-iMac-11.attlocal.net>
2017-04-07 17:06:00 -0700

    hola
    dfsgsadgafhashha

---
+++ src/HarryPotterRunner.java
@@ -0,0 +1,16 @@
+
+public class HarryPotterRunner {
+	public static void main(String[] args) {
+		// 1. make harry potter
+	HarryPotter h= new HarryPotter();
+		// 2. become invisible
+	h.makeInvisible(true);
+	// 3. spy on professor snape
+	h.spyOnSnape();	
+	// 4. become visible again
+	h.makeInvisible(false);	
+	// 5. cast a “stupefy” spell
+	h.castSpell("stupefy");
+	}
+
+}
