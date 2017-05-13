a7af9ec7635e091f9bf937db08a7f2dade128465
Airborne4 <league@WTS-iMac-11.attlocal.net>
2017-05-12 17:00:17 -0700

    cyfxjxg
    fcjjx jxj

--- src/HarryPotter.java
+++ src/HarryPotter.java
@@ -1,4 +1,5 @@
-public class HarryPotter {
+public class 
+ {
 
 	private boolean cloakOn;
 	
---
+++ src/SmurfRunner.java
@@ -0,0 +1,12 @@
+
+public class SmurfRunner {
+public static void main(String[] args) {
+	Smurf handy= new Smurf("Handy");
+System.out.println(handy.getName());
+handy.eat();
+
+Smurf pappa= new Smurf("Pappa");
+System.out.println(pappa.getName());
+System.out.println(pappa.getHatColor());
+}
+}
