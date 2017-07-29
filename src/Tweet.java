ac7f7af3dcb382804a3495c506c8cfb8bb2ca766
CactusCat <wjmorris2004@gmail.com>
2017-07-28 17:03:01 -0700

    ukgyegyviyfefsz
    uigygukdfsguykasdh

---
+++ src/Tweet.java
@@ -0,0 +1,66 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class Tweet implements ActionListener {
+	JFrame j = new JFrame();
+	JPanel jp = new JPanel();
+	JTextField input = new JTextField(20);
+	JButton jb = new JButton("Search the TWITTERVERSE!");
+	JTextField newtweet = new JTextField(20);
+
+	public Tweet() {
+
+		j.add(jp);
+		jp.add(input);
+		jp.add(jb);
+		jp.add(newtweet);
+		j.pack();
+		j.setVisible(true);
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jb.addActionListener(this);
+		
+
+	}
+
+	public static void main(String[] args) {
+		new Tweet();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("tweet tweet");
+		String ltweet = getLatestTweet(input.getText());
+		newtweet.setText(ltweet);
+	}
+
+	private String getLatestTweet(String searchingFor) {
+
+		Twitter twitter = new TwitterFactory().getInstance();
+		AccessToken accessToken = new AccessToken("2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+				"vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+		twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN", "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+		twitter.setOAuthAccessToken(accessToken);
+
+		Query query = new Query(searchingFor);
+		try {
+			QueryResult result = twitter.search(query);
+			return result.getTweets().get(0).getText();
+		} catch (Exception e) {
+			System.err.print(e.getMessage());
+			return "What the heck is that?";
+		}
+	}
+
+}
---
+++ src/pigLatinTranslator.java
@@ -0,0 +1,109 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import sun.applet.Main;
+
+public class pigLatinTranslator implements ActionListener {
+	JFrame j = new JFrame();
+	JPanel jp = new JPanel();
+	JTextField answer = new JTextField(20);
+	JButton jb = new JButton("Translate!");
+	JTextField input = new JTextField(20);
+
+	public pigLatinTranslator() {
+
+		j.add(jp);
+		jp.add(input);
+		jp.add(jb);
+		jp.add(answer);
+		j.setVisible(true);
+		j.pack();
+		jb.setToolTipText("Click to translate!");
+		j.setDefaultCloseOperation(j.EXIT_ON_CLOSE);
+		answer.setToolTipText("Your translated word!");
+		input.setToolTipText("Insert word here");
+	jb.addActionListener(this);
+	}
+
+	public static void main(String[] args) {
+		new pigLatinTranslator();
+	}
+
+	  private static boolean isLetter(char c) {
+		    return ( (c >='A' && c <='Z') || (c >='a' && c <='z') );
+
+	  }
+
+	  private static String pigWord(String word) {
+		    int split = firstVowel(word);
+		    return word.substring(split)+"-"+word.substring(0, split)+"ay";
+		  }
+
+	  public String translate(String s) {
+		    String latin = "";
+		    int i = 0;
+		    while (i<s.length()) {
+
+		      // Take care of punctuation and spaces
+		      while (i<s.length() && !isLetter(s.charAt(i))) {
+		        latin = latin + s.charAt(i);
+		        i++;
+		      }
+
+		      // If there aren't any words left, stop.
+		      if (i>=s.length()) break;
+
+		      // Otherwise we're at the beginning of a word.
+		      int begin = i;
+		      while (i<s.length() && isLetter(s.charAt(i))) {
+		        i++;
+		      }
+
+		      // Now we're at the end of a word, so translate it.
+		      int end = i;
+		      latin = latin + pigWord(s.substring(begin, end));
+		    }
+		    return latin;
+		  }
+
+		  /**
+		   * Method to find the index of the first vowel in a word.
+		   * @param word The word to search
+		   * @return The index of the first vowel
+		   */
+		  private static int firstVowel(String word) {
+		    word = word.toLowerCase();
+		    for (int i=0; i<word.length(); i++)
+		      if (word.charAt(i)=='a' || word.charAt(i)=='e' ||
+		          word.charAt(i)=='i' || word.charAt(i)=='o' ||
+		          word.charAt(i)=='u')
+		        return i;
+		    return 0;
+		  }
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource()==jb) {
+				answer.setText(translate(input.getText()));
+			}
+			
+		}
+
+
+
+}
+	  
+	  
+
+
+
+
+
+
+
+
